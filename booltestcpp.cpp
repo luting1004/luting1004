621de1f4d8c1df0b699bbeefb37d10fb1056544e
婷婷卢 <111483804+luting1004@users.noreply.github.com>
2022-10-26 18:00:11 +0800

    Add files via upload

---
+++ booltestcpp.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>//string 头文件；
+using namespace std;
+
+int main(){
+	//查看bool数据类型
+
+	/*
+
+	bool flag = true;
+	cout << flag<<endl;
+
+	//查看bool的内存空间
+	cout << sizeof(flag)<<endl;
+3
+
+	int a = 10;
+	cout << "请给整型赋值" << endl;
+	cin >> a;
+	cout << "修改后a的值为" <<a<< endl;
+
+	float f = 3.12;
+	cout << "请给浮点型赋值" << endl;
+
+	cin >> f;
+	cout << "赋值后的f为" << f << endl;
+
+	char ch = 'a';
+	cout << "请给字符运算符赋值" << endl;
+
+	cin >> ch;
+	cout << "赋值后运算符为：" << ch << endl;
+
+
+	string str = "qqqq";
+	cout << "请给字符串赋值" << endl;
+
+	cin >> str;
+	cout << "赋值后str=" << str << endl;
+	*/
+	
+
+	bool q = false;
+	cout << "请赋值" <<q<< endl;
+	cin >>q;
+	cout << "赋值后q=" << q << endl;
+
+
+	system("pause");
+	return 0;
+
+
+
+
+}
