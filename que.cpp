3956234e64b7f981db555460456757639907ff0c
婷婷卢 <111483804+luting1004@users.noreply.github.com>
2022-10-26 10:05:54 +0800

    Add files via upload

---
+++ que.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+ 
+using namespace std;
+#define Day 7
+
+
+int main(){
+	cout << "hello world"<<endl;
+	cout << "我是一个好人";
+	cout << "hell";
+	//单行注释
+	
+
+	//多行注释
+ 
+
+
+	int a = 10;
+	cout << "a=" << a << endl;
+
+	cout << "一周一共有" <<Day<< "天";
+
+	system("pause");
+	return 0;
+}
+/*
+int main1(){
+cout << "我是";
+system("pause");
+return 0;
+
+
+}
+*/
