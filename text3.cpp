01df8a3078a5a24691ffda959ec7dd3979fcf252
婷婷卢 <111483804+luting1004@users.noreply.github.com>
2022-10-28 16:49:48 +0800

    Add files via upload

---
+++ text3.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+/*
+
+int main(){
+
+int score = 0;
+cout << "输入你的分数" << endl;
+cin >> score;
+
+cout << "你的分数为:" << score << endl;
+
+if (score>=600)
+{
+cout << "你可以上好大学" << endl;
+
+}
+else
+{
+cout << "你上二本" << endl;
+}
+
+system("pause");
+return 0;
+
+}
+*/
